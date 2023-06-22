34fae0dba3eceaee04a96745189f250ebd45ec9a
Memnock92 <c.matuszak@outlook.com>
2023-06-22 14:19:52 +0200

    Scrollbar
    - added scrollbar to mainwindow
    - Changed the starting size of the window to fit better with the scrollbar

--- JSON Editor.py
+++ JSON Editor.py
@@ -4,7 +4,7 @@ from json import dumps, load
 from tkinter import ttk
 
 
-def dict_ent(root: dict, root_frame):
+def dict_ent(root: dict, root_frame, canvas):
     for index, (key, value) in enumerate(root.items()):
         if isinstance(value, dict):
             frm = tk.Frame(
@@ -12,8 +12,8 @@ def dict_ent(root: dict, root_frame):
                 padx=5,
                 pady=5,
             )
-            frm.pack(expand=1, fill=BOTH)
-            dict_ent(value, frm)
+            frm.pack(expand=1, fill=tk.BOTH)
+            dict_ent(value, frm, canvas)
         else:
             entry_frame = tk.Frame(
                 root_frame,
@@ -22,26 +22,30 @@ def dict_ent(root: dict, root_frame):
                 padx=5,
                 pady=5,
             )
-            entry_frame.pack(expand=1, fill=BOTH)
+            entry_frame.pack(expand=1, fill=tk.BOTH)
 
-            input_var = StringVar(value=dumps(value))
-            L1 = Label(entry_frame, text=key, anchor="w").pack(
+            input_var = tk.StringVar(value=dumps(value))
+            tk.Label(entry_frame, text=key, anchor="w").pack(
                 expand=1,
-                fill=BOTH,
+                fill=tk.BOTH,
                 padx=0,
                 pady=5,
-                side=LEFT,
+                side=tk.LEFT,
             )
-            E1 = Entry(
+            tk.Entry(
                 entry_frame,
                 textvariable=input_var,
             ).pack(
                 expand=0,
-                fill=BOTH,
+                fill=tk.BOTH,
                 ipadx=110,
                 pady=2,
-                side=LEFT,
+                side=tk.LEFT,
             )
+
+
+def on_canvas_configure(event):
+    canvas.configure(scrollregion=canvas.bbox("all"))
 
 
 with open("tds-server.json") as file:
@@ -49,14 +53,23 @@ with open("tds-server.json") as file:
 
 window = tk.Tk()
 window.title("Editor")
-window.geometry("1024x800")
+window.geometry("600x800")
 
-frm = tk.Frame(
-    window,
-)
+frm = tk.Frame(window)
+frm.pack(expand=True, fill=tk.BOTH)
 
-frm.pack(expand=1, fill=BOTH)
+canvas = tk.Canvas(frm)
+canvas.pack(side="left", fill=tk.BOTH, expand=True)
 
-dict_ent(server_config, frm)
+scrollbar = ttk.Scrollbar(frm, orient="vertical", command=canvas.yview)
+scrollbar.pack(side="right", fill="y")
+
+canvas.configure(yscrollcommand=scrollbar.set)
+canvas.bind("<Configure>", on_canvas_configure)
+
+inner_frame = tk.Frame(canvas)
+canvas.create_window((0, 0), window=inner_frame, anchor="nw")
+
+dict_ent(server_config, inner_frame, canvas)
 
 window.mainloop()
