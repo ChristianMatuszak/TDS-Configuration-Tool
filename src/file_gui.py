576a8c0a1af96e3a3023e458402de02cbd0b6114
Memnock92 <c.matuszak@outlook.com>
2023-09-27 10:47:52 +0200

    Updated doc_source and moved files
    - Added docs_source to gitignore
    - all moved files from root to src

---
+++ src/file_gui.py
@@ -0,0 +1,225 @@
+import tkinter as tk
+from tkinter import ttk
+from tkinter import PhotoImage
+from file_editor import *
+from file_io import *
+from win_service import *
+import click
+from version import VERSION
+
+FRAME_PADDING = 5
+
+
+class App(ttk.Frame):
+    """Class for creating the individual frames and buttons as well as the basic functions for the application
+
+    Args:
+        configuration_path (path): Location for the configuration file from TDS
+        schema (schema): file for dynamic creation of the application
+        window (frame): Mainframe of the application
+    """
+
+    def __init__(self, parent, configuration_path, schema, window):
+        ttk.Frame.__init__(self)
+        self.parent = parent
+
+        configuration_path, tds = read_tds(configuration_path)
+
+        info_frame = ttk.LabelFrame(self, text="File Info")
+        info_frame.pack(side=tk.TOP, anchor=tk.CENTER)
+
+        config_path_frame = ttk.Frame(info_frame)
+        config_path_frame.pack(
+            side=tk.TOP,
+            fill="both",
+            anchor=tk.W,
+            pady=(0, FRAME_PADDING),
+            padx=FRAME_PADDING,
+        )
+
+        config_path_label = tk.Label(
+            config_path_frame,
+            text="Config Path:",
+            padx=FRAME_PADDING,
+            pady=FRAME_PADDING,
+            anchor=tk.E,
+            width=12,
+        )
+        config_path_label.pack(side=tk.LEFT, padx=FRAME_PADDING)
+
+        config_values_label = tk.Label(
+            config_path_frame,
+            text=configuration_path,
+            padx=FRAME_PADDING,
+            pady=FRAME_PADDING,
+            anchor=tk.W,
+        )
+        config_values_label.pack(side=tk.LEFT, expand=1, fill="both")
+
+        last_modified_frame = ttk.Frame(info_frame)
+        last_modified_frame.pack(
+            side=tk.TOP, anchor=tk.W, pady=(0, FRAME_PADDING), padx=5
+        )
+
+        last_modified_label = tk.Label(
+            last_modified_frame,
+            text="Last Modified:",
+            padx=FRAME_PADDING,
+            pady=FRAME_PADDING,
+            anchor=tk.E,
+            width=12,
+        )
+        last_modified_label.pack(side=tk.LEFT, padx=FRAME_PADDING)
+
+        last_modified_values_label = tk.Label(
+            last_modified_frame,
+            text=last_modified(configuration_path)
+            if configuration_path is not None
+            else "Not Loaded",
+            padx=FRAME_PADDING,
+            pady=FRAME_PADDING,
+            anchor=tk.W,
+        )
+        last_modified_values_label.pack(side=tk.LEFT)
+
+        service_frame = ttk.Frame(info_frame)
+        service_frame.pack(
+            expand=1,
+            side=tk.TOP,
+            fill="both",
+            anchor=tk.W,
+            pady=(0, FRAME_PADDING),
+            padx=FRAME_PADDING,
+        )
+
+        service_label = tk.Label(
+            service_frame,
+            text="Service:",
+            padx=FRAME_PADDING,
+            pady=FRAME_PADDING,
+            anchor=tk.E,
+            width=12,
+        )
+        service_label.pack(side=tk.LEFT, padx=FRAME_PADDING)
+
+        service_values_label = tk.Label(
+            service_frame,
+            padx=FRAME_PADDING,
+            pady=FRAME_PADDING,
+            anchor=tk.W,
+        )
+        service_values_label.pack(side=tk.LEFT)
+
+        def poll_service(window: tk.Tk, value_label: tk.Label):
+            """Windows service polled every second
+
+            Args:
+                window (tk.Tk): Mainframe for the output
+                value_label (tk.Label): Location of the output
+            """
+            if service_running():
+                value_label.config(text="Running", anchor="e")
+            else:
+                value_label.config(text="Stopped", anchor="e")
+            window.after(1000, poll_service, window, value_label)
+
+        poll_service(self, service_values_label)
+
+        frm = tk.Frame(self)
+        frm.pack(expand=True, fill=tk.BOTH)
+
+        show_in_explorer_button = ttk.Button(
+            config_path_frame,
+            text="Show in Explorer",
+            command=lambda: show_in_explorer(configuration_path),
+        )
+        show_in_explorer_button.pack(
+            side="right", anchor="e", padx=(FRAME_PADDING, FRAME_PADDING)
+        )
+        tds_browser = ttk.Button(
+            service_frame,
+            text="Show in Browser",
+            command=lambda: open_browser(tds),
+        )
+        tds_browser.pack(side="right", anchor="e", padx=(FRAME_PADDING, FRAME_PADDING))
+        start_button = ttk.Button(
+            service_frame,
+            text="Start Service",
+            command=start_service,
+        )
+        start_button.pack(side="right", anchor="e", padx=(FRAME_PADDING, FRAME_PADDING))
+
+        stop_button = ttk.Button(
+            service_frame, text="Stop Service", command=stop_service
+        )
+        stop_button.pack(side="right", anchor="e", padx=(50, FRAME_PADDING))
+
+        bottom_frame = ttk.Frame(self)
+        bottom_frame.pack(
+            side="bottom",
+            fill="both",
+            padx=(5, 5),
+            pady=FRAME_PADDING,
+        )
+
+        help_button = ttk.Button(bottom_frame, text="Help", command=open_help)
+        help_button.pack(side=tk.LEFT, fill="both", padx=FRAME_PADDING)
+
+        version_number = ttk.Label(bottom_frame, text=f"Version: {VERSION}")
+        version_number.pack(side=tk.LEFT, fill="x", pady=FRAME_PADDING)
+
+        save_button = ttk.Button(
+            bottom_frame,
+            text="Save",
+            command=lambda: save_handler(self.tab_state, configuration_path, parent),
+        )
+        save_button.pack(side=tk.RIGHT, fill=tk.BOTH, padx=(400, 5))
+
+        def close_app():
+            """Function to prevent accidental closure and query whether you are sure with yes, no and save and exit buttons"""
+            window.attributes("-disabled", True)
+
+            confirm_handler(window)
+
+        self.tab_state = populate_tabs(read_schema(schema), frm, tds)
+        parent.protocol("WM_DELETE_WINDOW", close_app)
+
+    def run(self):
+        """function for the mainloop of the application"""
+        try:
+            self.parent.mainloop()
+        except Exception as e:
+            messagebox.showerror("Exception", f"{e}")
+
+
+@click.command()
+@click.option(
+    "--configuration",
+    help="Path to the tds-server location.",
+)
+@click.option(
+    "--schema",
+    help="create the schema.json file if the created file was not found.",
+)
+def main(configuration, schema):
+    window = tk.Tk()
+    window.title("TDS Configuration-Tool")
+    window.resizable(width=0, height=0)
+
+    icon = PhotoImage(file=resource_path("resources/favicon.png"))
+    window.iconphoto(False, icon)
+
+    window.tk.call("source", resource_path("resources/azure.tcl"))
+    window.tk.call("set_theme", "light")
+
+    style = ttk.Style()
+    style.configure("TLabel", backgounrd="Grey")
+    style.configure("TLabelframe", background="dodgerblue")
+
+    app = App(window, configuration, schema, window)
+    app.pack(fill="both", expand=True)
+    app.run()
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/file_io.py
@@ -0,0 +1,111 @@
+import json
+import os
+import datetime as dt
+from tkinter import StringVar
+from tkinter.filedialog import askdirectory, askopenfile
+from os.path import exists
+import subprocess
+import sys
+
+
+def read_tds(configuration_path):
+    """read the tds-server.json file
+
+    Returns:
+        return: return dict for the json file
+    """
+    if configuration_path is not None and exists(configuration_path):
+        # if not os.path.isfile(configuration_path):
+        #    return None, None
+        with open(configuration_path, "r") as f:
+            return (configuration_path, json.load(f))
+
+    elif configuration_path is None:
+        result_path = subprocess.run(
+            [
+                "C:/Program Files/Tessonics/TDS2/tds-server.exe",
+                "show-config",
+                "origin",
+            ],
+            capture_output=True,
+        )
+        if result_path.returncode == 0:
+            configuration_path = result_path.stdout.strip().decode()
+            with open(configuration_path, "r") as f:
+                return (configuration_path, json.load(f))
+    else:
+        return None, None
+
+
+def read_schema(default_schema):
+    """read the schema.json file
+
+    Returns:
+        return: return dict for the json file
+    """
+    # 1. Load from command line
+    if default_schema is not None:
+        return json.loads(default_schema)
+
+    # 2. Load from TDS Application
+    else:
+        result = subprocess.run(
+            ["C:/Program Files/Tessonics/TDS2/tds-server.exe", "show-config", "schema"],
+            capture_output=True,
+        )
+        if result.returncode == 0:
+            return json.loads(result.stdout)
+
+
+def save_tds(config, configuration_path):
+    """function to save chaged values in the tds-server file"""
+
+    with open(configuration_path, "w") as f:
+        json.dump(config, f, indent=4)
+
+
+def last_modified(configuration_path):
+    """get the last modified time on the tds-server file
+
+    Returns:
+        date: returns date and time when the file was last edited
+    """
+    last_modified = dt.datetime.fromtimestamp(os.path.getmtime(configuration_path))
+
+    return last_modified.strftime("%d.%m.%Y - %H:%M")
+
+
+def show_in_explorer(configuration_path):
+    """funtion to show the tds-server.json file in the explorer"""
+
+    folder = os.path.dirname(configuration_path)
+    os.startfile(folder)
+
+
+def open_explorer(entry: StringVar, viewer_type):
+    """function to select new path for files and directories
+
+    Args:
+        entry (StringVar): current path
+        viewer_type (_type_): check if it is a file if not it has to be a directory
+    """
+    if viewer_type == "text-edit-browse-file":
+        parent_directory = os.path.dirname(entry.get())
+        new_file = askopenfile(initialdir=parent_directory)
+        if new_file is not None:
+            entry.set(new_file.name)
+    else:
+        parent_directory = os.path.dirname(entry.get())
+        new_directory = askdirectory(initialdir=parent_directory)
+        if new_directory != "":
+            entry.set(new_directory)
+
+
+def resource_path(relative_path):
+    """Get absolute path to resource, works for dev and for PyInstaller"""
+    try:
+        # PyInstaller creates a temp folder and stores path in _MEIPASS
+        base_path = sys._MEIPASS
+    except Exception:
+        base_path = os.path.abspath(".")
+    return os.path.join(base_path, relative_path)
