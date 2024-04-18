fe02f5de8f030840b51dd5846ade61994b6afedb
Felix Heitmann <felix@tessonics.com>
2024-04-18 10:43:38 +0200

    trying to fix version issue

--- src/embed-version.py
+++ src/embed-version.py
@@ -2,5 +2,7 @@ import versiontag
 
 current_version = versiontag.get_version()
 
+print("Embedding version: ", current_version)
+
 with open("version.py", "w") as f:
     f.write(f'VERSION = "{current_version}"')
